2b7d3e6db96d28ff7f95d77a479b945ac5b4d2af
Chi Phan <cphan@wesleyan.edu>
2023-10-07 02:13:32 -0400

    added fishing hook and line renderer

---
+++ FishingSanta/Assets/Scripts/FishingRod.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingRod : MonoBehaviour
+{
+    public float rodSpeed = 2.0f;
+    public float reelSpeed = 1.0f;
+    private float x_pos = 0.8f;
+    private float y_pos = 5.0f;
+    public LineRenderer rod;
+    public GameObject hook;
+    private bool isCasting = false;
+    public GameObject present = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rod = GetComponent<LineRenderer>();
+        rod.positionCount = 2;
+        rod.SetPosition(0, new Vector3 (x_pos, y_pos, 0f));
+        rod.SetPosition(1, new Vector3 (x_pos, y_pos - 2, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))  
+        {
+            ExtendRod();
+        }
+        else
+        {
+            ReelInRod();
+        }
+
+        // if (present != null)
+        // {
+        //     // Move the hooked present with the hook
+        //     present.transform.position = hook.transform.position;
+
+        //     // Despawn the fish when it reaches the top
+        //     if (hook.transform.position.y >= 10.0f)
+        //     {
+        //         Destroy(present);
+        //         present = null;
+        //     }
+        // }
+    }
+
+    private void ExtendRod()
+    {
+        Vector3 currentPosition = rod.GetPosition(1);
+        if (currentPosition.y > -5.0f) 
+        {
+            currentPosition.y -= rodSpeed * Time.deltaTime;
+            rod.SetPosition(1, currentPosition);     
+        }
+        hook.transform.position = rod.GetPosition(1);
+    }
+
+    private void ReelInRod() 
+    {
+        Vector3 currentPosition = rod.GetPosition(1);
+        if (currentPosition.y < 5.0f) {
+            currentPosition.y += rodSpeed * Time.deltaTime;
+            rod.SetPosition(1, currentPosition);
+        }
+        hook.transform.position = rod.GetPosition(1);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Present") && isCasting)
+        {
+            // Hook the present when it touches the hook
+            present = other.gameObject;
+
+            // Move the hooked present with the hook
+            present.transform.position = hook.transform.position;
+
+            // Despawn the present when it reaches the top
+            if (hook.transform.position.y >= 5.0f)
+            {
+                Destroy(present);
+                present = null;
+            }
+        }
+    }
+}
